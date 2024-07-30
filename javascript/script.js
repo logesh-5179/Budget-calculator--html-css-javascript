f656b44f5c4ae4d6163e9cfa3a1457ab05fc2af1
logesh-5179 <logeshmb5179@gmail.com>
2024-07-30 20:57:48 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    const start = [
-        { type: 'Income', name: 'Salary', amount: 500 },
-        { type: 'Expense', name: 'Rent', amount: 100 }
-    ];
-    start.forEach(transaction => addTransaction(transaction));
     document.getElementById('tran-form').addEventListener('submit', function(e) {
         e.preventDefault();
-    const type = document.getElementById('type').value;
+ const type = document.getElementById('type').value;
         const name = document.getElementById('name').value;
         const amount = document.getElementById('amount').value;
-        if (type === '' || name === '' || amount === '') {
-            alert('Please enter');
+    if (type === '' || name === '' || amount === '') {
+            alert('Please enter all fields');
             return;
         }
+
         const transaction = { type, name, amount: parseFloat(amount) };
         addTransaction(transaction);
         document.getElementById('tran-form').reset();
@@ -22,14 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function addTransaction(transaction) {
     const tr = document.createElement('tr');
-    
-    tr.innerHTML = `
-        <td>${transaction.type}</td>
-        <td>${transaction.name}</td>
-        <td>$${transaction.amount}</td>
-        <td><span class="delete-btn">Delete</span></td>
-    `;
+
+    tr.innerHTML = `<td>${transaction.type}</td>
+                    <td>${transaction.name}</td>
+                    <td>$${transaction.amount.toFixed(2)}</td>
+                    <td><span class="delete-btn">Delete</span></td> `;
+
     document.getElementById('tran-list').appendChild(tr);
+    
     tr.querySelector('.delete-btn').addEventListener('click', function() {
         tr.remove();
     });
